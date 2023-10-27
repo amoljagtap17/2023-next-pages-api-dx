1150dc224fc1e627ed0d4f7daeddb342e8c7feee
amoljagtap17 <amoljagtap17@gmail.com>
2023-10-27 19:14:19 +0530

    next-pages-api-dx: add abstract and base service class

---
+++ src/app/services/abstract.service.ts
@@ -0,0 +1,17 @@
+import { Document } from "mongoose";
+
+export abstract class AbstractService<T extends Document> {
+  abstract find(filters?: any): Promise<T[]>;
+
+  abstract findById(id: string): Promise<T | null>;
+
+  abstract findOne(conditions: any): Promise<T | null>;
+
+  abstract findMultipleByIds(ids: string[]): Promise<T[]>;
+
+  abstract create(data: Partial<T>): Promise<T>;
+
+  abstract findByIdAndUpdate(id: string, data: Partial<T>): Promise<T | null>;
+
+  abstract findByIdAndRemove(id: string): Promise<T | null>;
+}
---
+++ src/app/services/base.service.ts
@@ -0,0 +1,41 @@
+import { Document } from "mongoose";
+import { CrudOperations } from "../utils";
+import { AbstractService } from "./abstract.service";
+
+export class BaseService<T extends Document> extends AbstractService<T> {
+  private crud: CrudOperations<T>;
+
+  constructor(model: any) {
+    super();
+
+    this.crud = new CrudOperations(model);
+  }
+
+  async find(filters?: any): Promise<T[]> {
+    return this.crud.find(filters);
+  }
+
+  async findById(id: string): Promise<T | null> {
+    return this.crud.findById(id);
+  }
+
+  async findOne(conditions: any): Promise<T | null> {
+    return this.crud.findOne(conditions);
+  }
+
+  async findMultipleByIds(ids: string[]): Promise<T[]> {
+    return this.crud.findMultipleByIds(ids);
+  }
+
+  async create(data: Partial<T>): Promise<T> {
+    return this.crud.create(data);
+  }
+
+  async findByIdAndUpdate(id: string, data: Partial<T>): Promise<T | null> {
+    return this.crud.findByIdAndUpdate(id, data);
+  }
+
+  async findByIdAndRemove(id: string): Promise<T | null> {
+    return this.crud.findByIdAndRemove(id);
+  }
+}
